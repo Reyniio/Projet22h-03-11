cfc8b7489fc382feba26cc47cdf764b431c4dc71
Erwan Leboucher <erwanleboucher@gmail.com>
2016-11-03 17:57:10 +0100

    op copié collé

--- FourmiliereV3.java
+++ FourmiliereV3.java
@@ -32,7 +32,7 @@ public class FourmiliereV3
         }
 
         while (ligF!=SORTIE_1 ||ligF!=SORTIE_2 ){
-            char move = reader.next().charAt(0);
+            char move = sc.next().charAt(0);
                     switch(move) {
                         case 'N':
                             colF -= colF;
